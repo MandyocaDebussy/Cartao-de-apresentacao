d76e77cd1f5d9738f9133a4aaa8774b81d457e62
amanda debussy <amanda.debussy.di@gmail.com>
2023-04-28 13:33:35 -0300

    new

---
+++ ko-fi/script.js
@@ -0,0 +1,9 @@
+function ModeDark() {
+    let html = document.querySelector("body");
+    html.classList.toggle("darkmode");
+}
+
+/*
+let main = document.querySelector("main");
+main.classList.toggle("darkmain");
+*/
